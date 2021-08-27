195411384afdba15656e4e960750a8232264497d
carlsondultra <carlsondultra@users.noreply.github.com>
2021-08-27 15:40:19 -0400

    Linking Homepage component using React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
+import HomePage from '../pages/HomePage';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const debug =
   process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
@@ -10,7 +13,11 @@ const debug =
 function App() {
   return (
     <StyletronProvider value={engine} debug={debug} debugAfterHydration>
-      
+      <Router>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Router>
     </StyletronProvider>
   );
 }
