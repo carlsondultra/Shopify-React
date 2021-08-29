dda63dd8bdede26082f164c9a2c08b2e4fc0c7dd
carlsondultra <carlsondultra@users.noreply.github.com>
2021-08-29 15:06:41 -0400

    Fetching Product ID upon clicking on product

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/shopContext'
 
 const ProductPage = () => {
+
+    let { id } = useParams();
+    const { fetchProductWithId, addItemToCheckout, product } = useContext(ShopContext)
+
+    useEffect(() => {
+        fetchProductWithId(id)
+        return () => {
+
+        }
+    }, [ fetchProductWithId, id ])
+
+    if(!product) return <div>Loading Product</div>
+
     return (
         <div>
             Product Page
+            {id}
         </div>
     )
 }
